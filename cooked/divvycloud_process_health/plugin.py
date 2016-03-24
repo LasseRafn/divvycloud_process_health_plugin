5990e23f14b1d17778ab3fd0de3b8f63fad4f544
Brian Johnson <brian@divvycloud.com>
2016-03-24 15:28:35 -0400

    Adding associated worker queues

--- cooked/divvycloud_process_health/plugin.py
+++ cooked/divvycloud_process_health/plugin.py
@@ -4,6 +4,10 @@ from DivvySession import DivvySession
 from DivvyBlueprints import get_ondemand_queue
 from DivvyUtils.flask_helpers import JsonResponse
 from DivvyPlugins.plugin_metadata import PluginMetadata
+from collections import defaultdict
+from DivvyRQ.DivvyRQ import DivvyRQProxyWorker
+
+
 
 class metadata(PluginMetadata):
     version = '1.0'
@@ -24,12 +28,16 @@ def get_queues_status():
     """
     Retrieve a list of DivvyCloud processes
     """
-
     process_list = []
 
     # Retrieve redis connection data from our ondemand queue
     redis_queue = get_ondemand_queue().redis_queue
     connection = redis_queue.connection
+
+    workers_by_queue = defaultdict(set)
+    for worker in DivvyRQProxyWorker.all(connection=connection):
+        for queue_name in worker.queue_names():
+            workers_by_queue[worker.name].add(queue_name)
 
     # Get a list of divvy cloud processes
     processes = connection.keys('divvy.process.*')
@@ -40,12 +48,15 @@ def get_queues_status():
         host = process.split(":")[1]
         pid = process.split(":")[2]
         ttl = connection.ttl(process)
+        shortname, _, _ = host.partition(".")
+        full_proc_name = "%s.%s" % (shortname,pid)
         process_list.append({
             "process_type" :  proc_type
             ,"host" : host
             ,"last_checkin_time" :  time_stamp
             ,"PID" : pid
             ,"TTL" : ttl
+            ,"queues" : list(workers_by_queue[full_proc_name])
         })
 
     return JsonResponse({'process_list' : process_list})
